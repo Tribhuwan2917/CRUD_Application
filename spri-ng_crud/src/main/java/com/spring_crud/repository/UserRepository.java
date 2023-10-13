e28ab949278240a953947eb9a163725b6da201fa
Tribhuwan Pal <138198609+Tribhuwan2917@users.noreply.github.com>
2023-10-13 17:55:00 +0530

    crud application has been completed

--- spri-ng_crud/src/main/java/com/spring_crud/repository/UserRepository.java
+++ spri-ng_crud/src/main/java/com/spring_crud/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.spring_crud.repository;
 
-public class UserRepository {
+import com.spring_crud.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+
 }
--- spri-ng_crud/src/main/java/com/spring_crud/service/UserService.java
+++ spri-ng_crud/src/main/java/com/spring_crud/service/UserService.java
@@ -1,5 +1,12 @@
 package com.spring_crud.service;
 
+import java.util.List;
+
 public interface UserService {
+    public List<UserResponse> getAllUser();
+    public UserResponse getUser(Integer userId);
+    public Integer addUser(UserResponse userResponse);
+    public Integer deleteUser(Integer userId);
+    public Integer updateUser(UserResponse userResponse);
 
 }
