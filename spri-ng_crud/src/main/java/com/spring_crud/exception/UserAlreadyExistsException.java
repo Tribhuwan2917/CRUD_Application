7497c2c842f2959cbcf1a93c2800897ced257b84
Tribhuwan Pal <138198609+Tribhuwan2917@users.noreply.github.com>
2023-10-13 18:06:35 +0530

    crud application has been completed

--- spri-ng_crud/src/main/java/com/spring_crud/exception/UserAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.spring_crud.exception;
-
-public class UserAlreadyExistsException extends RuntimeException{
-    public UserAlreadyExistsException(String message)
-    {
-        super(message);
-    }
-}
--- spri-ng_crud/src/main/java/com/spring_crud/exception/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.spring_crud.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message)
-    {
-        super(message);
-    }
-}
